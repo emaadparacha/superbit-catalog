5314cda4480459c69251dbed6a9c1c37fa9793a5
Emaad Paracha <emaadparacha@hotmail.com>
2025-07-21 07:22:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Combined Sources Explorer", layout="wide")
 
-st.title("SuperBIT Sources Explorer")
+st.title("SuperBIT Targets and Sources Explorer 🔭")
 
 PARQUET_URL = 'http://hen.astro.utoronto.ca/data/combined_sources.parquet'
 
